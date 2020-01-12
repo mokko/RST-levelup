ce981f6b2a9dc42c94c863c546c24bf1578e1918
Maurice Mengel <mauricemengel@gmail.com>
2020-01-13 00:22:35 +0100

    more elaborate example; some cleanup; index_with_attribute first dirty version

--- bin/excelTool.py
+++ bin/excelTool.py
@@ -3,10 +3,10 @@ Proof of concept version for ExcelTool
 
 (1) create an updatable index in Excel
 
-For out purposes, an index is 
+For our purposes, an index is 
 -a list of distinct values
--typically in alphabetical order
--plus the number of occurences of the values
+-typically in alphabetical order (but not necessarily)
+-plus the number of occurrences of the values
 -the data typically comes from an individual field, but I'd like to allow any xpath expression which includes
      ./sammlungsobjekt/maßangaben[@typ ='Ausgabe']
 
@@ -39,20 +39,22 @@ from openpyxl import Workbook, load_workbook
 #from cx_Freeze.samples.openpyxl.test_openpyxl import wb
 
 class ExcelTool: 
-    def __init__ (self, source):
+    def __init__ (self, source, xls_fn):
         self.ns = {
             'npx': 'http://www.mpx.org/npx', #npx is no mpx
-            'mpx': 'http://www.mpx.org/mpx', #npx is no mpx
+            'mpx': 'http://www.mpx.org/mpx', 
         }
         self.new_file=0
         self.tree = ET.parse(source)
+        self.wb=self._prepare_wb(xls_fn)
+        self.xls_fn=xls_fn
 
     #TODO
     def _xpath2core (self,xpath):
         '''this transformation is insufficient for a lot of expressions eg. involving [@attribute], but it'll do for the moment.
         '''
-        core=xpath.split(':')[1]
-        core=core.split('/')[0]
+        core=xpath.split('/')[-1]
+        core=core.split(':')[1].replace('[','').replace(']','').replace(' ','').replace('=','').replace('\'','')
         print ('xpath->core: ' + xpath + '->' + core)
         return core     
     
@@ -80,6 +82,12 @@ class ExcelTool:
             ws['A1']='GEWIMMEL'
         if ws['B1'].value is None:
             ws['B1']='HÄUFIGKEIT'
+        if ws['C1'].value is None:
+            ws['C1']='QUALI'
+        if ws['D1'].value is None:
+            ws['D1']='PREF (DE)'
+        if ws['E1'].value is None:
+            ws['E1']='PREF (EN)'
 
     def _prepare_wb (self, xls_fn):
         '''Read existing xls or make new one, return values in self'''
@@ -91,6 +99,20 @@ class ExcelTool:
             print ('File doesn\'t exist yet, making it ('+ xls_fn+')')
             self.new_file=1
             return Workbook()
+
+    def _col_to_zero (self,ws,col):    
+        '''
+        Set all existing values of a respective column to 0. Only header (row=1) remains unchanged.
+        
+        self._col_to_zero (ws, 'B')
+        '''
+        c=1 # 1-based line counter 
+        for each in ws[col]:
+            if c != 1: #IGNORE HEADER
+                #print (str(c)+': '+each.value)
+                each.value=0 # None doesn't work
+            c+=1
+        return c
 
 
     def _del_col (self, ws, col):
@@ -108,17 +130,23 @@ class ExcelTool:
             c+=1
         return c
 
-    
-    def index (self, xls_fn, xpath):
-        '''For xpath write index to xls sheet with keyword derived from xpath as name'''
-
-        wb=self._prepare_wb(xls_fn)
-        ws=self._prepare_ws(wb, xpath)
-        print ('ws.title: '+ws.title)
+    #TODO -> broken identity test
+    def index_with_attribute (self, xpath, quali): 
+        '''Based on xpath determine sheet name and write vocabulary index to that xls sheet 
+        
+        TODO: I would like to allow a qualifier that describes the term
+        Indien (Land) where "Land" is the qualifier for the term "Indien".
+        
+        But that's not so easy. I would need to prepare for the fact that the date could have both
+        Indien (Land) and Indien (Subkontinent), i.e. same value, but different qualifiers. 
+        Then the test for identity (_term_exist -> _term_exists_with_qualifier) needs to be different
+        
+        Maybe a cheap first version would not do that.
+        '''
+        ws=self._prepare_ws(self.wb, xpath)
+        #print ('ws.title: '+ws.title)
         self._prepare_header(ws)
-        self._del_col(ws, 'B')
-        #drop col B with occurrences every time we run a new index
-        #i.e. count only the most recent file
+        self._col_to_zero(ws, 'B')
 
         for term in self.tree.findall(xpath, self.ns): 
             row=self._term_exists(ws, term.text)
@@ -126,20 +154,47 @@ class ExcelTool:
                 #print ('term exists already: '+str(row))
                 cell='B'+str(row)
                 value=ws[cell].value
-                #print ('##########'+str(value))
                 if value=='':
                     ws[cell]=1
                 else:
                     ws[cell]=value+1
             else:
-                print ('term is new: '+ term.text)
+                print ('new term: '+ term.text)
+            
+            qu=term.get(quali)
+            print ('QUALI: '+ quali+': '+ str(qu))
+            self.insert_alphabetically(ws, term.text, qu)
+                
+
+        self.wb.save(self.xls_fn) 
+
+    def index (self, xpath):
+        '''Based on xpath determine sheet name and write vocabulary index to that xls sheet 
+        '''
+
+        ws=self._prepare_ws(self.wb, xpath)
+        print ('ws.title: '+ws.title)
+        self._prepare_header(ws)
+        self._col_to_zero(ws, 'B') #drop col B with occurrences every time we run a new index
+
+        for term in self.tree.findall(xpath, self.ns): 
+            row=self._term_exists(ws, term.text)
+            if row: 
+                #print ('term exists already: '+str(row))
+                cell='B'+str(row)
+                value=ws[cell].value
+                if value=='':
+                    ws[cell]=1
+                else:
+                    ws[cell]=value+1
+            else:
+                print ('new term: '+ term.text)
                 #append is only temporary, we want to sort it alphabetically for the most part #ws.append([term.text])  
                 #ws.append({'A' : term.text})
                 self.insert_alphabetically(ws, term.text)
                 #print (term.text+': '+str(self.insert_alphabetically (ws, term.text)))
-                
 
-        wb.save(xls_fn) # not necessary
+        self.wb.save(self.xls_fn) 
 
 
     def _line_alphabetically (self, ws, needle_term):
@@ -157,7 +212,7 @@ class ExcelTool:
         return c
 
 
-    def insert_alphabetically (self, ws, term): 
+    def insert_alphabetically (self, ws, term, quali=None): 
         '''
         inserts term into column A of worksheet ws after the first existing term
         
@@ -173,6 +228,8 @@ class ExcelTool:
         ws.insert_rows(line)
         ws['A'+str(line)]=term
         ws['B'+str(line)]=1
+        ws['C'+str(line)]=quali
+        
         #print ('...insert at line '+str(line))
 
 
@@ -206,5 +263,9 @@ class ExcelTool:
         #wb.save(xls_fn)
 
 if __name__ == '__main__': 
-    t=ExcelTool ('data/WAF55/20190927/2-MPX/levelup.mpx')
-    t.index('index.xlsx', "./mpx:sammlungsobjekt/mpx:sachbegriff")
+    t=ExcelTool ('data/WAF55/20190927/2-MPX/levelup.mpx', 'index.xlsx')
+    t.index("./mpx:sammlungsobjekt/mpx:sachbegriff")
+    t.index_with_attribute("./mpx:sammlungsobjekt/mpx:geogrBezug","bezeichnung")
+    t.index("./mpx:sammlungsobjekt/mpx:geogrBezug[@bezeichnung = 'Ethnie']")
+    t.index("./mpx:sammlungsobjekt/mpx:geogrBezug[@bezeichnung = 'Land']")
+    
