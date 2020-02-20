4ae869df6d8410170e5280542f4ecdc7cfce0f8b
Maurice Mengel <mauricemengel@gmail.com>
2020-02-20 08:38:26 +0100

    tif_finder.py: fixes and beautification

--- bin/tif_finder.py
+++ bin/tif_finder.py
@@ -2,8 +2,13 @@
 USAGE:
     tif_finder.py -u path/to/dir: makes new cache (not typical update)
     tif_finder.py -s needle: just report found files
-    tif_finder.py -x excel_fn: without -t argument it just reports found files
-    tif_finder.py -x excel_fn -t target_dir: copy found files to target dir
+
+    tif_finder.py -s needle -t target_dir: copy found tifs for needle to target_dir.
+
+    tif_finder.py -x excel_fn: just reports found tifs
+    tif_finder.py -x excel_fn: -t target_dir: copy found tifs to target dir
+    
+    tif_finder.py -S: show cache
 '''
 import os, sys, argparse
 
@@ -14,10 +19,11 @@ from Tif_finder import Tif_finder
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('-u', '--update_cache', required=False)
-    parser.add_argument('-s', '--search', required=False)
-    parser.add_argument('-x', '--xls', required=False)
-    parser.add_argument('-t', '--target_dir', required=False)
+    parser.add_argument('-u', '--update_cache')
+    parser.add_argument('-s', '--search')
+    parser.add_argument('-x', '--xls')
+    parser.add_argument('-t', '--target_dir')
+    parser.add_argument('-S', '--show_cache', action='store_true')
 
     args = parser.parse_args()
 
@@ -25,11 +31,15 @@ if __name__ == "__main__":
 
     if args.update_cache is not None:
         t.update_cache(args.update_cache)
-    elif args.search is not None:
+    elif args.show_cache:
+        t.show()
+    elif args.search is not None and args.target_dir is not None:
+        t.search (args.search, args.target_dir)
+    elif args.search is not None and not args.target_dir:
         t.search(args.search)
-    elif args.target_dir is not None:
+    elif args.search_xls is not None and args.target_dir is not None:
         t.search_xls (args.xls, args.target_dir)
-    elif args.xls is not None:
+    elif args.xls is not None and not args.target_dir:
         t.search_xls(args.xls)
     else:
         raise ValueError ('Unknown command line argument')
--- lib/Tif_finder.py
+++ lib/Tif_finder.py
@@ -5,15 +5,18 @@ from os.path import expanduser
 #from ntpath import splitext
 
 '''
-    We want to find certain tif images by filename. Sometimes we have a single identNr, sometimes we have a 
-    multiple of them in an excel file. We could even have a mpx file with lots of tifs. Let's tacke these cases
-    one by one.
+    We want to find certain tif images by filename. Sometimes we have a single needle (identNr), sometimes 
+    we have multiple needles in an excel file. We could even have a mpx file with lots of needles. 
+    Let's tacke these cases one by one.
     
-    First we need to scan a certain directory recursively and save the result into memory or to json file.
+    First we need to scan a certain directory recursively and save the result into memory and to json cache file.
     
-    We need to delete the json file; for now we can do that outside of the program. home/.tif_finder.json
+    Let's use NIX style:
+        $home/.tif_finder.json
     
-    Should we build a filter for the file extension into the program? Like *.tif or *.tiff
+    We need to delete the json file; for now we can do that outside of the program. 
+    
+    Right now we filter for permanentely for .tif* extensions
 '''
 
 class Tif_finder:
@@ -31,37 +34,44 @@ class Tif_finder:
             print ('* No cache file not found!')
 
 
+    '''Starts a new cache everytimes it's called so not a usual update function'''
     def update_cache (self, target_dir):
         print ('* Updating cache: %s' % target_dir)
         if (not os.path.isdir (target_dir)):
             raise ValueError ("Target dir '%s' does not exist" % target_dir)
 
         self.cache={}
+        ext='tif' #using *tif*
 
         print ('* About to scan %s' % target_dir)
-        ext='doc'
         for path in Path(target_dir).rglob('*.'+ext+'*'):
             abs = path.resolve()
             base = os.path.basename(abs)
             (trunk,ext)=os.path.splitext(base)
+            print (' %s' % abs)
             #print (str(trunk))
             self.cache[str(abs)]=str(trunk)
 
-        print ('**Writing new cache file')
+        print ('* Writing new cache file')
         with open(self.cache_fn, 'w') as f:
             json.dump(self.cache, f)
 
 
-    '''simple search: reports path from cache that match the needle'''
-    def search (self, needle):
+    '''search: a simple search taking needle from command line, simply reporting matching path from cache'''
+    def search (self, needle, target_dir=''):
         print ("* Searching cache for needle '%s'" % needle)
         ret=[]
+        c=0
         for path in self.cache:
             #print ('%s->%s' % (path,self.cache[path]))
             if needle in self.cache[path]:
+                c=c+1
                 ret.append(path) 
                 print (path)
+                self._copy(path,target_dir)
+        print ('%s matches'% c)
         return ret
+
 
     '''Take needle from Excel file (first sheet, column A) and copy file to target_dir if specified; if not just report it'''
     def search_xls (self, xls_fn, target_dir=''):
@@ -78,9 +88,47 @@ class Tif_finder:
                 found=self.search(needle.value)
                 for f in found:
                     print ('   FOUND: %s' % f)
-                    if target_dir != '':
-                        print ('mv %s -> %s' %(f, target_dir))
-                        shutil.copy(f, target_dir) # copy2 attempts to preserve file info;
+                    self._copy(f,target_dir)
+
+
+    ''' TODO
+    foreach record in lvlup.mpx
+    1. identNr & objId
+    2. look into cache if you find a tif for that
+    3. look for mumeRecord which has TIFF signal and urheber
+        findall (multimediaobjekt[verknüpftesObjekt == objId])
+        dateiname
+        if TIFsignal and urhebFotograf
+            mulId
+            urhebFotograf
+        out_fn=archived/dateiname.urhebFotograf.VmulId.tif
+    
+    V indicates that association between that MM record and this tif is a guess; 
+    i.e. it's not certain this is a version of the same photo.
+    '''
+    def search_mpx (self, mpx_fn):
+        try: 
+            shutil.copy(cached_path, out_fn) # copy2 attempts to preserve file info; why not
+        except:
+            print("Unexpected error:", sys.exc_info()[0])
+
+
+    def show(self):
+        print ('*Displaying cache contents')
+        for item in self.cache:
+            print (' %s' % item)
+        print ('%i total number of found items' % len(self.cache))
+
+    #############
+
+
+    def _copy(self, source,target_dir):
+        if target_dir != '':
+            print ('cp %s -> %s' %(source, target_dir))
+            try:
+                shutil.copy(source, target_dir) # copy2 attempts to preserve file info;
+            except:
+                print ('File not found: %s' % source)
 
 
     '''Read existing xls and return workbook'''
@@ -95,32 +143,6 @@ class Tif_finder:
             return Workbook()
 
 
-    def search_mpx (self, mpx_fn):
-        ''' TODO
-        foreach record in lvlup.mpx
-        1. identNr & objId
-        2. look into cache if you find a tif for that
-        3. look for mumeRecord which has TIFF signal and urheber
-            findall (multimediaobjekt[verknüpftesObjekt == objId])
-            dateiname
-            if TIFsignal and urhebFotograf
-                mulId
-                urhebFotograf
-            out_fn=archived/dateiname.urhebFotograf.VmulId.tif
-        
-        V indicates that association between that MM record and this tif is a guess; 
-        i.e. it's not certain this is a version of the same photo.
-        '''
-
-
-        #Dont copy file again if it already exists at target
-
-        try: 
-            shutil.copy(cached_path, out_fn) # copy2 attempts to preserve file info; why not
-        except:
-            print("Unexpected error:", sys.exc_info()[0])
-    
-    
 if __name__ == "__main__":
     
     f=Tif_Finder()
