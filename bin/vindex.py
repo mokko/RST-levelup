3c2c898bc1f9e4838b92bb56a516f30df17cc234
Maurice Mengel <mauricemengel@gmail.com>
2020-04-01 08:48:28 +0200

    vindex: Updated, but not tested

--- bin/vindex.py
+++ bin/vindex.py
@@ -1,67 +1,59 @@
-''' 
-Frontend for ExcelTool to create a vocabulary index as Excel file
+""" Commandline Frontend for ExcelTool. 
 
-USAGE:
+Creates an "updatable" vocabulary index as Excel file
+
+USAGE
     vindex.py --conf path/to/vindex.json --input 20200116/2-MPX/levelup.mpx 
-    #creates output file, e.g. vindex.xlsx in same dir as json config file
-
-(1) create an "updatable" vocabulary index in Excel
 
 For our purposes, a vocabulary index is 
-a) a list of distinct values
-b) typically in alphabetical order (but not necessarily 100% alphabetical)
-c) it shows the number of occurrences of the values
-d) the data typically comes from an individual field, but I'd like to allow any xpath expression as source, such as:
-     ./sammlungsobjekt/maßangaben[@typ ='Ausgabe']
+a) a list of distinct values/terms
+b) typically in alphabetical order (but user could change order)
+c) it shows the frequency of values/terms
+d) the data typically comes from an individual field:
+    ./mpx:sammlungsobjekt/mpx:maßangaben
 
 What does "updatable" mean?
-The index is written to an excel file, a user can edit this file, and the next run is supposed to update the index 
-without overwriting or otherwise destroying the user input.
+The index is written to an excel file, a user can edit this file, and the next 
+run is supposed to update the index without overwriting or otherwise 
+destroying the user input.
 
+Excel Tool works with multiple files
+(1) Data source is an mpx file
+(2) A json configuration file that describes mpx elements and attributes to 
+    make indexes for
+(3) One kind of output is the voc-index in XLS
+(4) Another kind of output is translation table in XLS
+(5) Another kind of output is rewritten mpx source file where terms have been 
+    replaced according to pref terms in voc-index. The rewritten version is 
+    called "the fix" or rather a fixed mpx.
+
+ExcelTool doesn't handle applying the translations from the Excel sheet. I plan
+to apply those to LIDO directly since I don't want to overload the mpx format 
+with translations.    
+
+RECOMMENDED PATHS
 Given  directory structure: 
-    EM-SM/20200111/2-MPX/levelup.mpx
-    EM-SM/20200116/2-MPX/levelup.mpx
+    data/EM-SM/20200111/2-MPX/levelup.mpx #specific export
+    data/EM-SM/20200116/2-MPX/levelup.mpx #specific export
+    data/EM-SM/vindex.xslx                #for multiple exports of the same object set
+    data/vindex-conf.json                 #for multiple object sets
 
-The voc-index is supposed to be persistent over multiple exports. In the case above there is one from
-January 11 and another from January 16. So a good place for the vocabulary index file is
-    EM-SM/vindex.xslx
+The voc-index is supposed to be persistent over multiple exports. In the case 
+above there is one from January 11 and another from January 16. So a good place
+for the vocabulary index file is in project root (as shown above).
 
-Path situation:
-Configuration for the ExcelTool lies in a json file. It lies in the same directory as the result:
-    EM-SM/vindex.json
-    
-At the moment I use mpx for input, later I want to use LIDO for input.
-
-Thinking out loud:    
-Output goes into the same directory the config file. How do we determine the source? We can parse all
-subdirectories automatically. But we probably only want to work on the latest one. Or we can specify the 
-newest file manually. Let's start with a manual version. It might be enough. We can make it go automatically
-later.
-
-Example config:
-- JSON doesn't allow comments
-
+XAMPLE CONF    
 {
-    "source":"data/WAF55/20190927/2-MPX/levelup.mpx", #TODO     
-    "out_fn":"vindex.xlsx",
     "tasks":[
         {"index":"./mpx:sammlungsobjekt/mpx:sachbegriff"},
         {"index_with_attribute": ["./mpx:sammlungsobjekt/mpx:geogrBezug","bezeichnung"]}, 
-        {"index":"./mpx:sammlungsobjekt/mpx:geogrBezug[@bezeichnung = 'Ethnie']"},
-        {"index":"./mpx:sammlungsobjekt/mpx:geogrBezug[@bezeichnung = 'Land']"}
+        {"attribute_index":["./mpx:sammlungsobjekt/mpx:geogrBezug/@bezeichnung" "verantwortlich"]}
     ]
 }
-'''
-import sys, os
-if os.getlogin() == 'M-MM0002':
-    sys.path.append ('C:/Users/M-MM0002/Documents/PY/RST-lvlup/lib')
-
-elif os.getlogin() == 'User':
-    sys.path.append ('C:/Users/User/eclipse-workspace/RST-Lvlup/RST-levelup/lib')
-
-else:
-    print ('Never get here:' + os.getlogin())
-
+"""
+import sys 
+import os
+sys.path.append (os.path.join(__file__,'../../lib'))
 from ExcelTool import ExcelTool
 
 if __name__ == "__main__":
@@ -69,7 +61,6 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('-c', '--conf', required=True)
     parser.add_argument('-i', '--input', required=True)
+    parser.add_argument('-x', '--xlsdir', required=False)
     args = parser.parse_args()
-            
-    #print ('ARGV ' + sys.argv[1])
-    t=ExcelTool.from_conf (args.conf,args.input)
+    t=ExcelTool.from_conf (args.conf,args.input, args.xlsdir)
